7080de9a8f4acc9b0670eece376a15470ed2c9c0
25LP6886 25LP6886 <25LP6886@admins-iMac.local>
2025-09-19 14:21:03 +0800

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,58 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { useState } from "react";
+import { StepOne } from "./_feature/StepOne";
+import { StepTwo } from "./_feature/StepTwo";
+import "./index.css";
+import { StepThree } from "./_feature/StepThree";
+import { StepFour } from "./_feature/StepFour";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const getSavedStep = () => {
+    if (typeof window !== "undefined") {
+      const saved = localStorage.getItem("currentStep");
+      return saved ? Number(saved) : 1;
+    }
+  };
+  const [step, setStep] = useState(getSavedStep);
+  const saveStep = (next) => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("currentStep", next);
+    }
+    setStep(next);
+  };
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const nextStep = () => {
+    saveStep(step + 1);
+  };
+
+  const backStep = () => {
+    if (step === 1) return;
+    saveStep(step - 1);
+  };
+
+  const backtostep = () => {
+    if (step === 4) {
+      saveStep(step - 3);
+    }
+  };
+  return (
+    <>
+      {step === 1 && <StepOne HandleNextStep={() => nextStep()} />}
+
+      {step === 2 && (
+        <StepTwo
+          HandleBackStep={() => backStep()}
+          HandleNextStep={() => nextStep()}
+        />
+      )}
+      {step === 3 && (
+        <StepThree
+          HandleBackStep={() => backStep()}
+          HandleNextStep={() => nextStep()}
+        />
+      )}
+      {step === 4 && <StepFour backtostep={() => backtostep()} />}
+    </>
   );
 }
